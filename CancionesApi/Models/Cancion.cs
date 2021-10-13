703241512b30f633f758990d40a87b5eeebbebcd
Nayelydaza20 <nayelydaza3@gmail.com>
2021-10-13 13:10:52 -0400

    se agrego

--- CancionesApi/Models/Cancion.cs
+++ CancionesApi/Models/Cancion.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CancionesApi.Models
 {
     public class Cancion
     {
+        [Key]
+        public string Nombre { get; set; }
+        [Required( ErrorMessage = "escriba de nuevo")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Letra { get; set; }
+        [Url]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Enlace { get; set; }
     }
 }
