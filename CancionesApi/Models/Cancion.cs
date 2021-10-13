94feaca7f2f1450e43135e8f8bf9735566be32e1
Nayelydaza20 <nayelydaza3@gmail.com>
2021-10-13 13:37:13 -0400

    ultima p

--- CancionesApi/Models/Cancion.cs
+++ CancionesApi/Models/Cancion.cs
@@ -4,9 +4,10 @@ namespace CancionesApi.Models
 {
     public class Cancion
     {
-        [Key]
+        [Key] 
+        
         public string Nombre { get; set; }
-        [Required( ErrorMessage = "escriba de nuevo")]
+        [Required(ErrorMessage = "escriba de nuevo")]
         public string Autor { get; set; }
         [Required(ErrorMessage = "{0} es requerido")]
         [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
